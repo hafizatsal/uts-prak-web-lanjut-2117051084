00045b5d0e5529d573a0cfee2b897c9d41e3bcf0
hafizatsal <hafiz.atsal@gmail.com>
2023-10-28 19:04:37 +0700

    routes views

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Controllers\Home;
 use CodeIgniter\Router\RouteCollection;
-
+use App\Controllers\UserController;
 /**
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/profile/(:any)/(:any)/(:any)', 'Home::profile');
--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,8 +4,18 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-    public function index(): string
+    public function profile($nama = "", $kelas = "", $npm = "")
     {
-        return view('welcome_message');
+        // Parameter dari URL diambil dan disimpan dalam variabel-variabel yang sesuai.
+        // Di sini, $nama akan menjadi "udin", $kelas akan menjadi "b", dan $npm akan menjadi "35564".
+        
+        $data = [
+            'nama' => $nama,
+            'kelas' => $kelas,
+            'npm' => $npm,
+        ];
+
+        // Mengganti view() dengan view('nama_file_view', $data) sesuai dengan file view yang digunakan
+        return view('profile', $data);
     }
-}
+};
